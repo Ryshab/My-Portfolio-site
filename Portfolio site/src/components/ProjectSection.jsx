1e9415f64bdfc689360e304fc34e946a819fb4be
Ryshab <rishabhydv2405@gmail.com>
2025-10-25 20:25:21 +0530

    project section

--- Portfolio site/src/components/ProjectSection.jsx
+++ Portfolio site/src/components/ProjectSection.jsx
@@ -34,7 +34,7 @@ const projects = [
 export const ProjectSection = () => {
     return (
         <section id="projects" className="py-24 px-4 relative">
-            <div className="container mx-auto max-w-5xl text-foreground">
+            <div className="container mx-auto max-w-7xl text-foreground">
                 <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
                     Featured <span className="text-primary">Projects</span>
                 </h2>
@@ -45,39 +45,37 @@ export const ProjectSection = () => {
                     {projects.map((project, key) => (
                         <div
                             key={key}
-                            className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+                            className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover flex flex-col min-h-[450px]"
                         >
-                            <div className="h-48 overflow-hidden">
+                            <div className="h-56 overflow-hidden">
                                 <img
                                     src={project.image}
                                     alt={project.title}
                                     className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
                             </div>
-                            <div className="p-4">
-                                <div className="flex flex-wrap gap-2 mb-4">
+                            <div className="p-4 flex flex-col flex-grow">
+                                <div className="flex flex-wrap gap-2 mb-6">
                                     {project.tags.map((tags) => (
                                         <span className="px-2 py-1 text-xs border font-medium rounded-full bg-secondary text-secondary-foreground">{tags} </span>
                                     ))}
                                 </div>
-                                <h3 className="text-xl font-semibold mb-1 ">{project.title} </h3>
-                                <p className="text-muted-foreground text-sm mb-4">{project.description} </p>
-                                <div className="flex justify-between items-center">
-                                    <div className="flex space-x-3">
-                                        <a
-                                            href={project.demoUrl}
-                                            target="_blank"
-                                            className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                                        >
-                                            <ExternalLink size={20} />
-                                        </a>
-                                        <a
-                                            href={project.githubUrl}
-                                            target="_blank"
-                                            className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                                        >
-                                            <Github size={20} />
-                                        </a>
-                                    </div>
+                                <h3 className="text-xl font-semibold mb-2 ">{project.title} </h3>
+                                <p className="text-muted-foreground text-sm mb-4 flex-grow">{project.description} </p>
+                                <div className="flex space-x-3 mt-auto">
+                                    <a
+                                        href={project.demoUrl}
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    >
+                                        <ExternalLink size={20} />
+                                    </a>
+                                    <a
+                                        href={project.githubUrl}
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    >
+                                        <Github size={20} />
+                                    </a>
                                 </div>
                             </div>
                         </div>
